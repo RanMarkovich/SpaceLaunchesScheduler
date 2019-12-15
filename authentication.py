0f63405f5269a4fdab77e614c84424d334053be2
RanMarkovich <ran@minutemedia.com>
2019-12-15 20:10:58 +0200

    refactor authentication.py and google_calendar.py to OOP

--- authentication.py
+++ authentication.py
@@ -1,18 +1,22 @@
-import json
-
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle
 
-scopes = ["https://www.googleapis.com/auth/calendar.events"]
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=scopes)
 
-# credentials = flow.run_local_server(port=0)
-#
-# with open('token.pkl', 'wb') as token:
-#     pickle.dump(credentials, token)
+class Auth:
+    def __init__(self):
+        self.scopes = ["https://www.googleapis.com/auth/calendar.events"]
 
-with open('token.pkl', 'rb') as token:
-    credentials = pickle.load(token)
-service = build('calendar', 'v3', credentials=credentials)
+    def get_auth(self):
+        flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=self.scopes)
+
+        # credentials = flow.run_local_server(port=0)
+        #
+        # with open('token.pkl', 'wb') as token:
+        #     pickle.dump(credentials, token)
+
+        with open('token.pkl', 'rb') as token:
+            credentials = pickle.load(token)
+        service = build('calendar', 'v3', credentials=credentials)
+        return service
