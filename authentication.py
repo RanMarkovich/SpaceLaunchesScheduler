0efad308c36bd6dfd6d2eb57166a3a190cbcbc65
RanMarkovich <ran@minutemedia.com>
2019-12-15 15:00:56 +0200

    added initial flow for auth getter in authentication.py

---
+++ authentication.py
@@ -0,0 +1,22 @@
+import json
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+import pickle
+
+scopes = ["https://www.googleapis.com/auth/calendar.events"]
+
+flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=scopes)
+
+# credentials = flow.run_local_server(port=0)
+#
+# with open('token.pkl', 'wb') as token:
+#     pickle.dump(credentials, token)
+
+with open('token.pkl', 'rb') as token:
+    credentials = pickle.load(token)
+service = build('calendar', 'v3', credentials=credentials)
+
+event = service.events().get(calendarId='markovich.org@gmail.com', eventId='2qd50sie64hn0d4el4gsm1r80m').execute()
+
+print(event)
