4706b961186351a2bb0df5f56b4e0df15d6fc956
RanMarkovich <ran@minutemedia.com>
2019-12-17 08:53:45 +0200

    add main func in app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from site_scraper import SiteScraper
+#!/usr/bin/env python3
+
 from google_calendar import GoogleCalendar
+from site_scraper import SiteScraper
 
-launch_data = SiteScraper().get_launch_data()
 
-i = 0
-for _ in range(4):
-    GoogleCalendar().create_event(launch_data, i)
-    i += 1
+def app_exe():
+    launch_data = SiteScraper().get_launch_data()
+
+    i = 0
+    for _ in range(4):
+        GoogleCalendar().create_event(launch_data, i)
+        i += 1
+
+
+if __name__ == '__main__':
+    app_exe()
